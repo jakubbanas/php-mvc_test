78712ed06e3253854e00d12ad90743e145b8be51
Jakub Banaś <kuba@iMac-Jakub.local>
2017-02-16 22:02:37 +0100

    url handling

--- app/core/App.php
+++ app/core/App.php
@@ -5,9 +5,17 @@ class App
 
     protected $controller = 'home';
     protected $method = 'index';
+    protected $params = [];
 
     public function __construct()
     {
+        var_dump($this->parseUrl());
+    }   
 
-    }    
+    public function parseUrl()
+    {
+        if(isset($_GET['url'])){
+            return $url = explode('/', filter_var(rtrim($_GET['url'], '/'), FILTER_SANITIZE_URL));
+        }
+    } 
 }
