0206d25f8a4f3f1f24f3cfcde43e382330abd777
Jakub Banaś <kuba@iMac-Jakub.local>
2017-02-21 06:06:13 +0100

    controllers handling

--- app/core/App.php
+++ app/core/App.php
@@ -3,19 +3,39 @@
 class App
 {
 
-    protected $controller = 'home';
-    protected $method = 'index';
-    protected $params = [];
+    private $controller = 'home';
+    private $method = 'index';
+    private $params = [];
+
+    protected function getController(){
+        return $this->controller;
+    }
+
+    protected function setController($newController){
+        return $this->controller = $newController;
+    }
 
     public function __construct()
     {
-        var_dump($this->parseUrl());
-    }   
+        $url = $this->parseUrl();
+        $controllerName = $url[0];
+
+        if (Controller::withNameExists($controllerName)) {
+            $this->setController($controllerName);
+        }
+
+        require_once Controller::getControllerPathByName($controllerName);
+    }
 
     public function parseUrl()
     {
-        if(isset($_GET['url'])){
-            return $url = explode('/', filter_var(rtrim($_GET['url'], '/'), FILTER_SANITIZE_URL));
+        if (isset($_GET['url'])) {
+            return $url = explode(
+                '/',
+                filter_var(
+                    rtrim($_GET['url'], '/'),
+                    FILTER_SANITIZE_URL)
+            );
         }
-    } 
+    }
 }
--- app/core/Controller.php
+++ app/core/Controller.php
@@ -1,6 +1,18 @@
 <?php
 
+define('CONTROLLERS_PATH', __DIR__ . '/../controllers/');
+
 class Controller
 {
-    
+
+    public static function withNameExists($name)
+    {
+        return file_exists(CONTROLLERS_PATH . $name . '.php');
+    }
+
+    public static function getControllerPathByName($name)
+    {
+        return CONTROLLERS_PATH . $name . '.php';
+    }
+
 }
